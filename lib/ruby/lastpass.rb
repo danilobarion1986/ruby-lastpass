fbfaac5df0c825ff5dc97d83665e3a13f33d13b7
Danilo Barion Nogueira <danilo.barion@vagas.com.br>
2018-11-09 14:07:31 -0200

    Initial commit

---
+++ lib/ruby/lastpass.rb
@@ -0,0 +1,8 @@
+require "ruby/lastpass/version"
+
+module Ruby
+  module Lastpass
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/ruby/lastpass/version.rb
@@ -0,0 +1,5 @@
+module Ruby
+  module Lastpass
+    VERSION = "0.1.0"
+  end
+end
