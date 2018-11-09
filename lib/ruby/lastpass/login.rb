19aeaab6cee58bb1af3301166a30a899fa9421b2
Danilo Barion Nogueira <danilo.barion@vagas.com.br>
2018-11-09 15:06:40 -0200

    Puts some instructions

--- lib/ruby/lastpass/login.rb
+++ lib/ruby/lastpass/login.rb
@@ -4,6 +4,8 @@ module Ruby
   module Lastpass
     class Login
       def initialize(username)
+        puts 'You need to install lastpass-cli first!'
+        puts 'Follow this instructions: https://github.com/lastpass/lastpass-cli'
         puts username
       end
     end
