533e6dfe3c89b7701ca15acee4359996f6e72833
Danilo Barion Nogueira <danilo.barion@vagas.com.br>
2018-11-11 12:38:41 -0200

    [WIP] experiments for Login class and main module

--- lib/ruby/lastpass.rb
+++ lib/ruby/lastpass.rb
@@ -7,8 +7,34 @@ module Ruby
   module Lastpass
     class Error < StandardError; end
 
+    attr_reader :logged, :result
+
     def self.login(username)
-      Login.new("User: #{username}")
+      @logged = Login.new(username)
+    rescue => ex
+      puts ex.message
+    end
+
+    def self.get(item)
+      @result = `#{list(item)}` # if logged?
+      info = { id: nil, username: nil }
+
+      unless @result.empty?
+        info[:id] = @result.match(/(\[id\: )([0-9]*)\]/)[2]
+        info[:username] = @result.match(/(\[username\: )(.*)(\])/)[2]
+      end
+
+      info
+    end
+
+    private
+
+    def self.list(item)
+      "lpass ls --long | grep #{item}"
+    end
+
+    def self.logged?
+      @logged
     end
   end
 end
--- lib/ruby/lastpass/login.rb
+++ lib/ruby/lastpass/login.rb
@@ -3,10 +3,23 @@
 module Ruby
   module Lastpass
     class Login
+      attr_reader :username
+
       def initialize(username)
-        puts 'You need to install lastpass-cli first!'
-        puts 'Follow this instructions: https://github.com/lastpass/lastpass-cli'
-        puts username
+        @username = username
+
+        if system('lpass -v')
+          raise Error, 'Cannot login!' unless login_succesful?
+        else
+          puts 'You need to install lastpass-cli first!'
+          puts 'Follow this instructions: https://github.com/lastpass/lastpass-cli'
+        end
+      end
+
+      private
+
+      def login_succesful?
+        system("lpass login #{username}")
       end
     end
   end
